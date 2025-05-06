9834b39ffd9638706bbd3bacfee0320b1dbd7b2f
Carlos <carlos.oliveirajr@hotmail.com>
2025-05-06 11:57:33 -0300

    DTO and layers structure

---
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscommerce.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Optional<Product> result = repository.findById(id); //Optional recebe resultado da buscar por id a partir do id
+		Product product = result.get(); // Product product recebe o resultado (objeto) que esta no Optional
+		ProductDTO dto = new ProductDTO(product); // productDTO recebe uma copia do Product que era resultado da busca por id
+		return dto; // e o método devolve um ProductDTO dto  para o controller que chamou o service ao invés de um Product da camada de entidades
+	}
+	
+}
