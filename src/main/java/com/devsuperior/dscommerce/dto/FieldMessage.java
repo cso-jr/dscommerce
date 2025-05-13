e3a79e776ac7d54b108b8326e79f570f6af60d63
Carlos <carlos.oliveirajr@hotmail.com>
2025-05-13 18:20:56 -0300

    Bean Validation

---
+++ src/main/java/com/devsuperior/dscommerce/dto/FieldMessage.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dscommerce.dto;
+
+public class FieldMessage {
+
+	private String fieldName;
+	private String messsage;
+	
+	
+	public FieldMessage(String fieldName, String messsage) {
+		super();
+		this.fieldName = fieldName;
+		this.messsage = messsage;
+	}
+	
+	public String getFieldName() {
+		return fieldName;
+	}
+	
+	public String getMesssage() {
+		return messsage;
+	}
+	
+	
+	
+}
