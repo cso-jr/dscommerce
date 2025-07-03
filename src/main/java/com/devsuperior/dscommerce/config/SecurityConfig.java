59033bf6f41f68438f68a57653f4738e840320db
Carlos <carlos.oliveirajr@hotmail.com>
2025-07-02 22:06:07 -0300

    BCrypt

--- src/main/java/com/devsuperior/dscommerce/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/dscommerce/config/SecurityConfig.java
@@ -3,12 +3,19 @@ package com.devsuperior.dscommerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 public class SecurityConfig {
 
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 		//Liberando provisioriamente os endpoints - que serao restringidos por rota ao inves de globalmente.
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
