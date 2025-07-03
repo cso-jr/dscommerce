f631c447738476f10e34f79698554cf59c3232ee
Carlos <carlos.oliveirajr@hotmail.com>
2025-07-02 21:32:09 -0300

    Spring Security, dependencies added, config package created

---
+++ src/main/java/com/devsuperior/dscommerce/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dscommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+		//Liberando provisioriamente os endpoints - que serao restringidos por rota ao inves de globalmente.
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable());
+		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+		return http.build();
+	}
+
+}
